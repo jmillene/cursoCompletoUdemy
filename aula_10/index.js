c42fd035cc4b1334fb98859c72f072f319ff94d9
Jessica <jmillene12@gmail.com>
2022-12-30 20:22:26 -0300

    strings

---
+++ aula_10/index.js
@@ -0,0 +1,4 @@
+const num1 = 10;
+const num2 = Number('40');
+
+console.log(num1+ num2);
---
+++ aula_12/index.js
@@ -0,0 +1,12 @@
+let letA = 'A'; 
+let letB = 'B';
+let letC = 'C';
+
+// const temp = letA
+// letA = letB;
+// letB = letC;
+// letC = temp;
+
+[letA, letB, letC] = [letB,letC, letA]
+
+console.log(letA, letB, letC);
